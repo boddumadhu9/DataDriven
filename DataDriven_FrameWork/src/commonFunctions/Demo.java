f1476270d80ad8b1a90cf39685a349b5fe2cedf6
user <user@DESKTOP-BFA4K3K>
2023-07-14 00:10:31 +0530

    New Class Commit

---
+++ DataDriven_FrameWork/src/commonFunctions/Demo.java
@@ -0,0 +1,23 @@
+package commonFunctions;
+
+public class Demo {
+public void add()
+{
+	int  a=234,b=56,c;
+	c=a+b;
+	System.out.println(c);
+}
+public void div()
+{
+	int  a=678,b=523,c;
+	c=a/b;
+	System.out.println(c);
+}
+public void mul()
+{
+	int  a=56789,b=3345,c;
+	c=a*b;
+	System.out.println(c);
+}
+
+}
