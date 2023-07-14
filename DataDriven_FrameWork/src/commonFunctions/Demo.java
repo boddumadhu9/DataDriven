53d8cb04871a897672bd3836b2b9b5b67ef8549e
user <user@DESKTOP-BFA4K3K>
2023-07-14 09:36:43 +0530

    Modified Values

--- DataDriven_FrameWork/src/commonFunctions/Demo.java
+++ DataDriven_FrameWork/src/commonFunctions/Demo.java
@@ -3,19 +3,19 @@ package commonFunctions;
 public class Demo {
 public void add()
 {
-	int  a=234,b=56,c;
+	int  a=767676,b=32,c;
 	c=a+b;
 	System.out.println(c);
 }
 public void div()
 {
-	int  a=678,b=523,c;
+	int  a=86786787,b=765786786,c;
 	c=a/b;
 	System.out.println(c);
 }
 public void mul()
 {
-	int  a=56789,b=3345,c;
+	int  a=4235435,b=543655,c;
 	c=a*b;
 	System.out.println(c);
 }
